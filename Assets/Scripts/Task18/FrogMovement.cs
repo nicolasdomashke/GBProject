a7a171ed02d2beb49ea119f63ea05715536f2144
nicolasdomashke <nicolasdomashke@gmail.com>
2024-09-23 18:34:38 +0300

    Jump update

--- Assets/Scripts/Task18/FrogMovement.cs
+++ Assets/Scripts/Task18/FrogMovement.cs
@@ -7,20 +7,28 @@ public class FrogMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private AnimationCurve animationGraph;
+    [SerializeField] private float jumpOffset;
+    [SerializeField] private Transform jumpSphere;
+    [SerializeField] private LayerMask groundMask;
     private Rigidbody2D rigidBody2D;
     private Vector2 movementVector;
     private Shooter shooter;
+    private bool isGrounded = false;
     void Awake()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         shooter = GetComponent<Shooter>();
     }
 
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(jumpSphere.position, jumpOffset, groundMask);
+    }
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         rigidBody2D.velocity = new Vector2(animationGraph.Evaluate(moveX) * speed, rigidBody2D.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
         }
